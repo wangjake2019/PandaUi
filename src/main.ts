4eeade2f1bd0353d77f239294c48e31c0ce679f2
PandaGoAdmin <18610165312@163.com>
2021-11-01 21:53:34 +0800

    all

--- src/main.ts
+++ src/main.ts
@@ -1,59 +1,28 @@
-import '/@/design/index.less';
-import 'virtual:windi-base.css';
-import 'virtual:windi-components.css';
-import 'virtual:windi-utilities.css';
-// Register icon sprite
-import 'virtual:svg-icons-register';
-import App from './App.vue';
 import { createApp } from 'vue';
-import { initAppConfigStore } from '/@/logics/initAppConfig';
-import { setupErrorHandle } from '/@/logics/error-handle';
-import { router, setupRouter } from '/@/router';
-import { setupRouterGuard } from '/@/router/guard';
-import { setupStore } from '/@/store';
-import { setupGlobDirectives } from '/@/directives';
-import { setupI18n } from '/@/locales/setupI18n';
-import { registerGlobComp } from '/@/components/registerGlobComp';
+import App from './App.vue';
+import router from './router';
+import { store, key } from './store';
+import { directive } from '/@/utils/directive';
+import { i18n } from '/@/i18n/index';
+import { globalComponentSize } from '/@/utils/componentSize';
 
-// Importing on demand in local development will increase the number of browser requests by around 20%.
-// This may slow down the browser refresh speed.
-// Therefore, only enable on-demand importing in production environments .
-if (import.meta.env.DEV) {
-  import('ant-design-vue/dist/antd.less');
-}
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import '/@/theme/index.scss';
+import mitt from 'mitt';
+import screenShort from 'vue-web-screen-shot';
+import VueGridLayout from 'vue-grid-layout';
 
-async function bootstrap() {
-  const app = createApp(App);
+const app = createApp(App);
+app
+	.use(router)
+	.use(store, key)
+	.use(ElementPlus, { i18n: i18n.global.t, size: globalComponentSize })
+	.use(i18n)
+	.use(screenShort, { enableWebRtc: false })
+	.use(VueGridLayout)
+	.mount('#app');
 
-  // Configure store
-  setupStore(app);
+app.config.globalProperties.mittBus = mitt();
 
-  // Initialize internal system configuration
-  initAppConfigStore();
-
-  // Register global components
-  registerGlobComp(app);
-
-  // Multilingual configuration
-  // Asynchronous case: language files may be obtained from the server side
-  await setupI18n(app);
-
-  // Configure routing
-  setupRouter(app);
-
-  // router-guard
-  setupRouterGuard(router);
-
-  // Register global directive
-  setupGlobDirectives(app);
-
-  // Configure global error handling
-  setupErrorHandle(app);
-
-  // https://next.router.vuejs.org/api/#isready
-  // await router.isReady();
-
-  app.mount('#app');
-}
-
-bootstrap();
+directive(app);
--- vite.config.ts
+++ vite.config.ts
@@ -1,107 +1,51 @@
-import type { UserConfig, ConfigEnv } from 'vite';
-import pkg from './package.json';
-import moment from 'moment';
-import { loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
-import { generateModifyVars } from './build/generate/generateModifyVars';
-import { createProxy } from './build/vite/proxy';
-import { wrapperEnv } from './build/utils';
-import { createVitePlugins } from './build/vite/plugin';
-import { OUTPUT_DIR } from './build/constant';
+import type { UserConfig } from 'vite';
+import { loadEnv } from './src/utils/viteBuild';
 
-function pathResolve(dir: string) {
-  return resolve(process.cwd(), '.', dir);
-}
-
-const { dependencies, devDependencies, name, version } = pkg;
-const __APP_INFO__ = {
-  pkg: { dependencies, devDependencies, name, version },
-  lastBuildTime: moment().format('YYYY-MM-DD HH:mm:ss'),
+const pathResolve = (dir: string): any => {
+	return resolve(__dirname, '.', dir);
 };
 
-export default ({ command, mode }: ConfigEnv): UserConfig => {
-  const root = process.cwd();
+const { VITE_PORT, VITE_OPEN, VITE_PUBLIC_PATH } = loadEnv();
 
-  const env = loadEnv(mode, root);
-
-  // The boolean type read by loadEnv is a string. This function can be converted to boolean type
-  const viteEnv = wrapperEnv(env);
-
-  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY, VITE_DROP_CONSOLE } = viteEnv;
-
-  const isBuild = command === 'build';
-
-  return {
-    base: VITE_PUBLIC_PATH,
-    root,
-    resolve: {
-      alias: [
-        {
-          find: 'vue-i18n',
-          replacement: 'vue-i18n/dist/vue-i18n.cjs.js',
-        },
-        // /@/xxxx => src/xxxx
-        {
-          find: /\/@\//,
-          replacement: pathResolve('src') + '/',
-        },
-        // /#/xxxx => types/xxxx
-        {
-          find: /\/#\//,
-          replacement: pathResolve('types') + '/',
-        },
-      ],
-    },
-    server: {
-      // Listening on all local IPs
-      host: true,
-      port: VITE_PORT,
-      // Load proxy configuration from .env
-      proxy: createProxy(VITE_PROXY),
-    },
-    build: {
-      target: 'es2015',
-      outDir: OUTPUT_DIR,
-      terserOptions: {
-        compress: {
-          keep_infinity: true,
-          // Used to delete console in production environment
-          drop_console: VITE_DROP_CONSOLE,
-        },
-      },
-      // Turning off brotliSize display can slightly reduce packaging time
-      brotliSize: false,
-      chunkSizeWarningLimit: 2000,
-    },
-    define: {
-      // setting vue-i18-next
-      // Suppress warning
-      __INTLIFY_PROD_DEVTOOLS__: false,
-      __APP_INFO__: JSON.stringify(__APP_INFO__),
-    },
-
-    css: {
-      preprocessorOptions: {
-        less: {
-          modifyVars: generateModifyVars(),
-          javascriptEnabled: true,
-        },
-      },
-    },
-
-    // The vite plugin used by the project. The quantity is large, so it is separately extracted and managed
-    plugins: createVitePlugins(viteEnv, isBuild),
-
-    optimizeDeps: {
-      // @iconify/iconify: The dependency is dynamically and virtually loaded by @purge-icons/generated, so it needs to be specified explicitly
-      include: [
-        '@iconify/iconify',
-        'ant-design-vue/es/locale/zh_CN',
-        'moment/dist/locale/zh-cn',
-        'ant-design-vue/es/locale/en_US',
-        'moment/dist/locale/eu',
-      ],
-      exclude: ['vue-demi'],
-    },
-  };
+const alias: Record<string, string> = {
+	'/@': pathResolve('./src/'),
+	'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
 };
+
+const viteConfig: UserConfig = {
+	plugins: [vue()],
+	root: process.cwd(),
+	resolve: { alias },
+	base: process.env.NODE_ENV === 'production' ? VITE_PUBLIC_PATH : './',
+	optimizeDeps: {
+		include: ['element-plus/lib/locale/lang/zh-cn', 'element-plus/lib/locale/lang/en', 'element-plus/lib/locale/lang/zh-tw'],
+	},
+	server: {
+		host: '0.0.0.0',
+		port: VITE_PORT,
+		open: VITE_OPEN,
+		proxy: {
+			'/gitee': {
+				target: 'https://gitee.com',
+				ws: true,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/gitee/, ''),
+			},
+		},
+	},
+	build: {
+		outDir: 'dist',
+		minify: 'esbuild',
+		sourcemap: false,
+		chunkSizeWarningLimit: 1500,
+	},
+	define: {
+		__VUE_I18N_LEGACY_API__: JSON.stringify(false),
+		__VUE_I18N_FULL_INSTALL__: JSON.stringify(false),
+		__INTLIFY_PROD_DEVTOOLS__: JSON.stringify(false),
+	},
+};
+
+export default viteConfig;
