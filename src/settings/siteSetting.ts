462daa5a5420e9d7389e9ebbd455fdafcc1ea4a1
PandaGoAdmin <18610165312@163.com>
2021-11-07 11:10:02 +0800

    logo

---
+++ src/settings/siteSetting.ts
@@ -0,0 +1,8 @@
+// github repo url
+export const GITHUB_URL = 'https://github.com/PandaGoAdmin/PandaX';
+
+// vue-vben-admin-next-doc
+export const DOC_URL = 'https://vvbin.cn/doc-next/';
+
+// site url
+export const SITE_URL = 'https://vvbin.cn/next/';
